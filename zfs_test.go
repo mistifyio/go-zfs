44a61d108b4a7ab61c8ea3b029c232b737042196
Manuel Mendez <mmendez534@gmail.com>
2016-10-20 17:10:19 -0400

    test: add nok helper to verify an error occurred

--- zfs_test.go
+++ zfs_test.go
@@ -41,6 +41,15 @@ func ok(tb testing.TB, err error) {
 	}
 }
 
+// nok fails the test if an err is nil.
+func nok(tb testing.TB, err error) {
+	if err == nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: expected error: %s\033[39m\n\n", filepath.Base(file), line)
+		tb.FailNow()
+	}
+}
+
 // equals fails the test if exp is not equal to act.
 func equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
