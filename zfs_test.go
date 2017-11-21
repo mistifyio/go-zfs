7cf9429546bce5a8c3055e8e1f3a49511c3c8f62
Manuel Mendez <mmendez534@gmail.com>
2017-11-20 23:04:41 -0500

    fix test nok error printing

--- zfs_test.go
+++ zfs_test.go
@@ -45,7 +45,7 @@ func ok(tb testing.TB, err error) {
 func nok(tb testing.TB, err error) {
 	if err == nil {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: expected error: %s\033[39m\n\n", filepath.Base(file), line)
+		fmt.Printf("\033[31m%s:%d: expected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 		tb.FailNow()
 	}
 }
